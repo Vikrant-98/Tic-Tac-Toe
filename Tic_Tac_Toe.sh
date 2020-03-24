70982e87ca6cd9a5bfe73e4dba6fa5bed132c2e1
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-24 12:14:19 +0530

    Empty space created

---
+++ Tic_Tac_Toe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+ar[0]='-'
+echo ""
+echo "|   ${ar[0]}       ${ar[0]}       ${ar[0]}   |"
+echo "|			|"
+echo "|			|"
+echo "|   ${ar[0]}       ${ar[0]}       ${ar[0]}   |"
+echo "|			|"
+echo "|			|"
+echo "|   ${ar[0]}       ${ar[0]}       ${ar[0]}   |"
+echo ""
