5b9b664a0a2dcd2df91edd77f26aabb49079025f
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-24 14:19:48 +0530

    Assignment added

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,12 +1,28 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 ar[0]='-'
+ar[1]='-'
+ar[2]='-'
+ar[3]='-'
+ar[4]='-'
+ar[5]='-'
+ar[6]='-'
+ar[7]='-'
+ar[8]='-'
 echo ""
-echo "|   ${ar[0]}       ${ar[0]}       ${ar[0]}   |"
+echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
 echo "|			|"
 echo "|			|"
-echo "|   ${ar[0]}       ${ar[0]}       ${ar[0]}   |"
+echo "|   ${ar[3]}       ${ar[4]}       ${ar[5]}   |"
 echo "|			|"
 echo "|			|"
-echo "|   ${ar[0]}       ${ar[0]}       ${ar[0]}   |"
+echo "|   ${ar[6]}       ${ar[7]}       ${ar[8]}   |"
 echo ""
+temp=$((RANDOM%2)) 
+if [[ $temp -eq 0 ]]
+then
+	assign='O'
+elif [[ $temp -eq 1 ]]
+then
+	assign='X'
+fi
