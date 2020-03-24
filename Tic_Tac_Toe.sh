f8a0c9ae6f8c6bc07b560b35ad9713c6dd83d8b4
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-24 18:34:56 +0530

    Change move Detail

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -25,7 +25,7 @@ then
 	computer='X'
 elif [[ $temp -eq 1 ]]
 then
-	Player_1='X'
+	player_1='X'
 	computer='O'
 fi
 toss=$((RANDOM%2))
@@ -38,7 +38,10 @@ then
         player=$computer
 	echo "It's Computer turn"
 fi
-
+count=0
+while [[ $count -ne 9 ]]
+do
+((count++))
 	echo ""
 	echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
 	echo "|                 	|"
@@ -53,8 +56,22 @@ fi
 	if [ ${ar[index-1]} == '-' ]
 	then
 		ar[index-1]=$player
+		if [ $player == 'O' ]
+		then
+			echo "Turn changed to X"
+		else
+			echo "Turn changed to O"
+		fi
 	else
 		echo "Invalid Entry!!!!!!!!!!!!!! Position is occupied"
+		((count--))
+		if [ $toss -eq 0 ]
+	        then
+        	        ((toss++))
+        	elif [ $toss -eq 1 ]
+        	then
+                	((toss--))
+        	fi
 	fi
 	echo ""
         echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
@@ -66,3 +83,14 @@ fi
         echo "|   ${ar[6]}       ${ar[7]}       ${ar[8]}   |"
         echo ""
 
+	if [ $toss -eq 0 ]
+	then
+		player=$computer
+		((toss++))
+	elif [ $toss -eq 1 ]
+	then
+		player=$player_1
+		((toss--))
+	fi
+
+done
