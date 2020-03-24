0fbfff15c9ad560a68b499f815e99402dcab8d29
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-24 18:04:08 +0530

    Choose the position for next move

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 ar[0]='-'
 ar[1]='-'
@@ -18,22 +18,51 @@ echo "|			|"
 echo "|			|"
 echo "|   ${ar[6]}       ${ar[7]}       ${ar[8]}   |"
 echo ""
-temp=$((RANDOM%2)) 
+temp=$((RANDOM%2))
 if [[ $temp -eq 0 ]]
 then
 	player_1='O'
-	player_2='X'
+	computer='X'
 elif [[ $temp -eq 1 ]]
 then
 	Player_1='X'
-	player_2='O'
+	computer='O'
 fi
 toss=$((RANDOM%2))
 if [[ $toss -eq 0 ]]
 then
         player=$player_1
+	echo "It'a Player_1 turn"
 elif [[ $toss -eq 1 ]]
 then
-        player=$player_2
+        player=$computer
+	echo "It's Computer turn"
 fi
+
+	echo ""
+	echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
+	echo "|                 	|"
+	echo "|                 	|"
+	echo "|   ${ar[3]}       ${ar[4]}       ${ar[5]}   |"
+	echo "|                 	|"
+	echo "|                 	|"
+	echo "|   ${ar[6]}       ${ar[7]}       ${ar[8]}   |"
+	echo ""
+
+	read -p "Enter the position where you want to insert $player" index
+	if [ ${ar[index-1]} == '-' ]
+	then
+		ar[index-1]=$player
+	else
+		echo "Invalid Entry!!!!!!!!!!!!!! Position is occupied"
+	fi
+	echo ""
+        echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
+        echo "|                 	|"
+        echo "|                 	|"
+        echo "|   ${ar[3]}       ${ar[4]}       ${ar[5]}   |"
+        echo "|                 	|"
+        echo "|                 	|"
+        echo "|   ${ar[6]}       ${ar[7]}       ${ar[8]}   |"
+        echo ""
 
