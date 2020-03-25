8acab6a024fe482f63ea167b17f5adc365ca9977
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-26 01:21:22 +0530

    Occupied the one of the corner

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,4 +1,43 @@
-#!/bin/bash 
+#!/bin/bash -x
+
+function Win_Check()
+{
+
+index1=0
+count_1=0
+while [ $count_1 -ne 3 ]
+do
+	if [[ ${ar[index1]} == $player && ${ar[index1+1]} == $player && ${ar[index1+2]} == $player ]]
+	then	
+		count_1=3
+		count=9
+	fi
+((count_1++))
+index1=$(($index1+3))	
+done
+index1=0
+count_1=0
+while [ $count_1 -ne 3 ]
+do
+	if [[ ${ar[index1]} == $player && ${ar[index1+3]} == $player && ${ar[index1+6]} == $player ]]
+	then
+		count_1=3
+		count=9
+	fi
+((count_1++))
+index1=$(($index1+1))
+done
+index1=0
+	if [[ ${ar[index1]} == $player && ${ar[index1+4]} == $player$ && ${ar[index1+8]} == $player ]]
+	then
+		count=9
+	elif [[ ${ar[index1+2]} == $player && ${ar[index1+4]} == $player && ${ar[index1+6]} == $player ]]
+	then
+		count=9
+	fi
+
+}
+ 
 ar[0]='-'
 ar[1]='-'
 ar[2]='-'
@@ -8,13 +47,7 @@ ar[5]='-'
 ar[6]='-'
 ar[7]='-'
 ar[8]='-'
-counter[0]=0
-counter[1]=0
-counter[2]=0
-counter[3]=0
-counter[4]=0
-counter[5]=0
-record=0
+invalid=0
 echo ""
 echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
 echo "|			|"
@@ -45,86 +78,26 @@ then
 	echo "It's Computer turn"
 fi
 count=0
-while [ $count -ne 9 ]
+while [[ $count -ne 9 ]]
 do
 ((count++))
-	echo ""
-	echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
-	echo "|                 	|"
-	echo "|                 	|"
-	echo "|   ${ar[3]}       ${ar[4]}       ${ar[5]}   |"
-	echo "|                 	|"
-	echo "|                 	|"
-	echo "|   ${ar[6]}       ${ar[7]}       ${ar[8]}   |"
-	echo ""
-
-	if [[ $toss -eq 1 ]]
+	if [ $toss == 1 ]
 	then
-		index1=0
-		count_1=0
-		while [ $count_1 -le 2 ]
-		do
-			((count_1++))
-			if [[ ${ar[index1]} == 'X' && ${ar[index1+1]} == 'X' && ${counter[0]} != 1 ]]
+		val=$(( $((RANDOM%5)) * 2 ))
+		if [[ $val -ne 4 && $val -lt 9 ]]
+		then
+			index=$val
+		elif [[ $invalid -eq 1 ]]
+		then			
+			((index++))
+			if [[ $index -gt 8 ]]
 			then
-				if [[ ${ar[record+1]} == '-' ]]
-				then
-					index=$(($record+1))
-				else
-					index=$(($record+2))
-				fi
-				counter[0]=1
-				count_1=3
-			elif [[ ${ar[index1+1]} == 'X' && ${ar[index1+2]} == 'X' && ${counter[1]} != 1 ]]
-			then
-				if [[ ${ar[record-1]} == '-' ]]
-				then
-					index=$(($record-1))
-				else
-					index=$(($record-2))
-				fi
-				counter[1]=1
-				count_1=3
-			elif [[ ${ar[index1]} == 'X' && ${ar[index1+2]} == 'X' && ${counter[2]} != 1 ]]
-			then
-					index=$(($(($index1+$index1+2))/2))
-				counter[2]=1
-				count_1=3
-			elif [[ ${ar[index1]} == 'O' && ${ar[index1+1]} == 'O' && ${counter[3]} != 1 ]]
-			then
-				if [[ ${ar[record+1]} == '-' ]]
-				then
-					index=$(($record+1))
-				else
-					index=$(($record+2))
-				fi
-				counter[3]=1
-				count_1=3
-			elif [[ ${ar[index1+1]} == 'O' && ${ar[index1+2]} == 'O' && ${counter[4]} != 1 ]]
-			then
-				if [[ ${ar[record-1]} == '-' ]]
-				then
-					index=$(($record-1))
-				else
-					index=$(($record-2))
-				fi
-				counter[4]=1
-				count_1=3
-			elif [[ ${ar[index1]} == 'O' && ${ar[index1+2]} == 'O' && ${counter[5]} != 1 ]]
-			then
-					index=$(($(($index1+$index1+2))/2))	
-				counter[5]=1
-				count_1=3
-			else
-				index=$((RANDOM%8))
+				index=0
+				invalid=0
 			fi
-		index1=$(($index1+3))
-		done
-
-	elif [[ $toss -eq 0 ]]
-	then
-		read -p "Enter the position where you want to insert $player" index
-		record=$index
+		fi
+	else
+	read -p "Enter the position where you want to insert $player" index
 	fi
 	if [[ ${ar[index]} == '-' ]]
 	then
@@ -136,15 +109,26 @@ do
 			echo "Turn changed to O"
 		fi
 	else
+		invalid=1
 		((count--))
-		if [ $toss -eq 0 ]
+		if [[ $toss -eq 0 ]]
 	        then
         	        ((toss++))
-        	elif [ $toss -eq 1 ]
+        	elif [[ $toss -eq 1 ]]
         	then
                 	((toss--))
         	fi
 	fi
+	echo ""
+        echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
+        echo "|                 	|"
+        echo "|                 	|"
+        echo "|   ${ar[3]}       ${ar[4]}       ${ar[5]}   |"
+        echo "|                 	|"
+        echo "|                 	|"
+        echo "|   ${ar[6]}       ${ar[7]}       ${ar[8]}   |"
+        echo ""
+
 	if [ $toss -eq 0 ]
 	then
 		player=$computer
@@ -154,14 +138,5 @@ do
 		player=$player_1
 		((toss--))
 	fi
-
+		Win_Check$()
 done
-	echo ""
-	echo "|   ${ar[0]}       ${ar[1]}       ${ar[2]}   |"
-	echo "|                 	|"
-	echo "|                 	|"
-	echo "|   ${ar[3]}       ${ar[4]}       ${ar[5]}   |"
-	echo "|                 	|"
-	echo "|                 	|"
-	echo "|   ${ar[6]}       ${ar[7]}       ${ar[8]}   |"
-	echo ""
